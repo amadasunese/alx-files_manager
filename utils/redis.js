a66f3cca81438ee23e5a40dc7dead934013f5091
Ese Amadasun <amadasunese@gmail.com>
2023-12-02 17:51:19 +0100

    updated

--- utils/redis.js
+++ utils/redis.js
@@ -1,51 +1,55 @@
+// Import the Redis library
 const redis = require('redis');
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient({
-      host: process.env.REDIS_HOST || 'localhost',
-      port: process.env.REDIS_PORT || 6379,
-    });
+    // Creating a Redis client
+    this.client = redis.createClient();
 
+    // Handling error event
     this.client.on('error', (err) => {
-      console.error('Redis connection error:', err);
+      console.error('Redis client error', err);
     });
+
+    // Connect to the Redis server
+    this.client.connect();
   }
 
+  // Check if the Redis connection is alive
   isAlive() {
-    return this.client.connected;
+    return this.client.isOpen;
   }
 
+  // Get a value from Redis
   async get(key) {
     try {
       const value = await this.client.get(key);
       return value;
     } catch (error) {
-      console.error('Error getting value from Redis for key:', key, error);
+      console.error('Redis get error', error);
       return null;
     }
   }
 
+  // Set a value in Redis with expiration
   async set(key, value, duration) {
     try {
-      await this.client.set(key, value, 'EX', duration);
-      return true;
+      await this.client.setEx(key, duration, value);
     } catch (error) {
-      console.error('Error setting value in Redis for key:', key, error);
-      return false;
+      console.error('Redis set error', error);
     }
   }
 
+  // Delete a key from Redis
   async del(key) {
     try {
       await this.client.del(key);
-      return true;
     } catch (error) {
-      console.error('Error deleting value from Redis for key:', key, error);
-      return false;
+      console.error('Redis delete error', error);
     }
   }
 }
 
+// Export an instance of RedisClient
 const redisClient = new RedisClient();
 module.exports = redisClient;
